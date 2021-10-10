b72bd6200417194c7f8a0b61245af6a59d528795
Sebastian Wywijas <swywijas@gmail.com>
2021-10-10 22:41:56 +0200

    Add project files.

---
+++ W2L8_Homework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace W2L8_Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
